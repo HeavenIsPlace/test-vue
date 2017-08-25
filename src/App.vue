4fe0981a79b84749de775b3d474bd27ba5a8f162
张小晴 <zhangxiaoqing@zhuojianchina.com>
2017-08-25 13:49:41 +0800

    试试

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <div class="leftPanel">
+      <ul>
+        <li><router-link to="/">用户统计</router-link></li>
+        <li><router-link to="/clockDetails">打卡明细</router-link></li>
+      </ul>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
